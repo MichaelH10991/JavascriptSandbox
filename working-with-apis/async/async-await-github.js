4405b226ff0aa0a29b6aa0224876d93158759f22
Michael Humphries <michaelhumphries1997@gmail.com>
2019-05-29 19:27:37 +0100

    adding async code

---
+++ working-with-apis/async/async-await-github.js
@@ -0,0 +1,22 @@
+async function showAvatar() {
+  let user = "mike-trt"
+
+  // read github user
+  let githubResponse = await fetch(`https://api.github.com/users/${user}`)
+  let githubUser = await githubResponse.json()
+
+  // show the avatar
+  let img = document.createElement("img")
+  img.src = githubUser.avatar_url
+  img.className = "promise-avatar-example"
+  document.body.append(img)
+
+  // wait 3 seconds
+  await new Promise((resolve, reject) => setTimeout(resolve, 3000))
+
+  return githubUser
+}
+
+showAvatar().catch(e => {
+  console.log(`There was an error :( ${e}`)
+})
--- working-with-apis/async/async-test.js
+++ working-with-apis/async/async-test.js
@@ -1,4 +1,4 @@
-const axios = require("axios")
+const axios = require("./node_modules/axios")
 function resolveAfter2Seconds() {
   return new Promise(resolve => {
     setTimeout(() => {
@@ -20,10 +20,10 @@ async function asyncCall() {
   this is code from 'fetch-a-thing.js' and it can be refactored into a series
   of async calls
  */
-let what = "posts"
-let n = 1
+// let what = "posts"
+// let n = 1
 // let data = { username: "example" }
-url = `https://jsonplaceholder.typicode.com/${what}`
+// let url = `https://jsonplaceholder.typicode.com/${what}`
 
 // old promise chain
 function getData(url) {
@@ -39,16 +39,22 @@ function getData(url) {
     })
 }
 
-// new async chain with axios to see the result
-// axios =  a Promise based HTTP client for the browser and node.js
-const getStuff = async url => {
+/* 
+  - new async chain with axios to see the result
+  - axios =  a Promise based HTTP client for the browser and node.js
+  - The word “async” before a function means one simple thing: a function always returns a promise.
+  - even if the return value is a non-promise
+*/
+
+let what = "posts"
+let url = `https://jsonplaceholder.typicode.com/${what}`
+
+async function getStuff(url) {
   console.log("about to fetch")
   console.log(`Fetching from ${url}`)
   return await axios.get(url)
 }
 
-let stuff = getStuff(url).catch(e => {
-  ;`There was an error: ${e}`
-})
-
-console.log(stuff)
+getStuff(url)
+  .then(res => console.log(res))
+  .catch(e => `There was an error: ${e}`)

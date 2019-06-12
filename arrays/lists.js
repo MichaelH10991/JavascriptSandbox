8196b597bf04f50976ebda490a86f55944131de2
Michael Humphries <michaelhumphries1997@gmail.com>
2019-06-12 19:56:38 +0100

    moving files around

---
+++ arrays/lists.js
@@ -0,0 +1,13 @@
+let array1 = ["a", "b", "c"]
+let array2 = ["d", "e", "f"]
+
+// concat method creates a new array with
+console.log(array1.concat(array2))
+
+// get the index of element
+let pos = array1.indexOf("b")
+
+// remove element from array
+array1.splice(1, 1)
+array1.splice(1, 1)
+console.log(array1)
--- lists/lists.js
+++
@@ -1,5 +0,0 @@
-let array1 = ["a", "b", "c"]
-let array2 = ["d", "e", "f"]
-
-// concat method creates a new array with
-console.log(array1.concat(array2))

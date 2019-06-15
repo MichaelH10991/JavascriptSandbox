c24623d7170d0628c6e3c7e3e377c7c9233bddf0
Michael Humphries <michaelhumphries1997@gmail.com>
2019-06-15 11:31:30 +0100

    adding timers.js

---
+++ node-sandbox/1.basics/index.js
@@ -0,0 +1,5 @@
+/* 
+Blocking - When the execution of javascript code has to wait for a non-javascript operation to finish, an API call for example.
+This happens because the event loop cannot continue running javascript whilst blocking operation is occuring
+
+*/
---
+++ node-sandbox/1.basics/timers.js
@@ -0,0 +1,39 @@
+/*
+This script executes a function wich takes a certain ammount of time which executes a callback which does nothing for 10 ms
+The delay is started streight after the first function is called so returns with a delay of how long the function took to
+execute
+*/
+
+const fs = require("fs")
+
+function someAsyncFunction(callback) {
+  fs.readFile("resources/hello.txt", callback)
+}
+
+const timeoutScheduled = Date.now()
+
+setTimeout(() => {
+  const delay = Date.now() - timeoutScheduled
+  console.log(`${delay}ms have passed since i was scheduled`)
+}, 100)
+
+// do someAsyncOperation which takes 95 ms to complete
+someAsyncFunction(() => {
+  const startCallback = Date.now()
+
+  // do something that will take 10ms...
+  while (Date.now() - startCallback < 10) {
+    // do nothing
+  }
+})
+
+/*
+When the event loop enters the poll phase, it has an empty queue (fs.readFile() has not completed), 
+so it will wait for the number of ms remaining until the soonest timer's threshold is reached. 
+While it is waiting 95 ms pass, fs.readFile() finishes reading the file and its callback which takes
+10 ms to complete is added to the poll queue and executed. When the callback finishes, there are 
+no more callbacks in the queue, so the event loop will see that the threshold of the soonest timer 
+has been reached then wrap back to the timers phase to execute the timer's callback. In this example,
+you will see that the total delay between the timer being scheduled and its callback being executed 
+will be 105ms.
+*/
